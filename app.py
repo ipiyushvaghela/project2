89c7f719c6a8a9022a2889a8bb3b420cad7d862d
Piyush Vaghela <ipiyushvaghela@gmail.com>
2022-12-12 19:41:12 +0530

    4 all change app to application

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ from flask import Flask
 
 application=Flask(__name__)
 
-app = application # cause aws need name as application
 
-@app.route('/')
+@application.route('/')
 def home():
     # return render_template('home.html')
     return "<h1>Hello it's working Fine</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=int("3000"))
+    application.run(debug=True,host='0.0.0.0',port=int("3000"))
