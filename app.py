6506213fc2d9dd8c99bde8ba8c245f9cc98b8d00
Piyush Vaghela <ipiyushvaghela@gmail.com>
2022-12-12 19:43:18 +0530

    4 all change app to application

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ application=Flask(__name__)
 
 @application.route('/')
 def home():
-    # return render_template('home.html')
-    return "<h1>Hello it's working Fine</h1>"
+    return "<h1>Hello it's working Fine on Elastic Beanstalk</h1>"
 
-if __name__ == '__main__':
-    application.run(debug=True,host='0.0.0.0',port=int("3000"))
+# if __name__ == '__main__':
+#     application.run(debug=True,host='0.0.0.0',port=int("3000"))
